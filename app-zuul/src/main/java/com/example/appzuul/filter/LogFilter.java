9acad3d315e840138bd27b708d957aa3b27769b2
wangwc <wwc953@163.com>
2020-07-23 10:32:13 +0800

    zuul 自定义Filter

---
+++ app-zuul/src/main/java/com/example/appzuul/filter/LogFilter.java
@@ -0,0 +1,53 @@
+package com.example.appzuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @Description: 自定义过滤器
+ * @author: wangwc
+ * @date: 2020/7/23 10:05
+ */
+@Component
+public class LogFilter extends ZuulFilter {
+    /**
+     * 过滤器的类型。有 pre、 route、 post、 error等几种取值
+     * @return
+     */
+    @Override
+    public String filterType() {
+        return FilterConstants.ROUTE_TYPE;
+    }
+
+    /**
+     * 指定过滤器的执行顺序
+     * @return
+     */
+    @Override
+    public int filterOrder() {
+        return FilterConstants.PRE_DECORATION_FILTER_ORDER;
+    }
+
+    /**
+     * 是否要执行
+     * @return
+     */
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext currentContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = currentContext.getRequest();
+        String remoteAddr = request.getRemoteAddr();
+        System.out.println("访问者IP：" + remoteAddr + "访问地址:" + request.getRequestURI());
+        return null;
+    }
+}
