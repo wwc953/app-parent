7d19ef0eab36bf1d933637651bc6aea71a699ee3
wangwc <wwc953@163.com>
2020-07-27 17:30:06 +0800

    redis cluster

---
+++ app-redis-cluster/src/main/java/com/example/apprediscluster/AppRedisClusterApplication.java
@@ -0,0 +1,13 @@
+package com.example.apprediscluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRedisClusterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRedisClusterApplication.class, args);
+    }
+
+}
---
+++ app-redis-cluster/src/test/java/com/example/apprediscluster/AppRedisClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apprediscluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppRedisClusterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
