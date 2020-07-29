669eaf28b7a22d409ff6e495fcc28c992ed18b43
wangwc <wwc953@163.com>
2020-07-29 08:39:15 +0800

    add zk module

---
+++ app-zk-lock/src/main/java/com/example/appzklock/AppZkLockApplication.java
@@ -0,0 +1,13 @@
+package com.example.appzklock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppZkLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppZkLockApplication.class, args);
+    }
+
+}
