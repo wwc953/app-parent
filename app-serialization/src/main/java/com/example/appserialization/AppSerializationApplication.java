d2869a98a1fa7148095394620cdab32cd552f045
wangwc <wwc953@163.com>
2020-11-01 07:57:38 +0800

    序列化

---
+++ app-serialization/src/main/java/com/example/appserialization/AppSerializationApplication.java
@@ -0,0 +1,12 @@
+package com.example.appserialization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSerializationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppSerializationApplication.class, args);
+
+    }
+}
