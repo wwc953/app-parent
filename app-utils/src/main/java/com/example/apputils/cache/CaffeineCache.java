da2dbd4d43cfcecd77fc7bbc11bd58454c948266
wangwc <wwc953@163.com>
2020-11-30 10:46:02 +0800

    CaffeineCache

---
+++ app-utils/src/main/java/com/example/apputils/cache/CaffeineCache.java
@@ -0,0 +1,12 @@
+package com.example.apputils.cache;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @Description: TODO
+ * @author: wangwc
+ * @date: 2020/11/30 10:45
+ */
+@Component
+public class CaffeineCache {
+}
---
+++ app-utils/src/main/java/com/example/apputils/cache/InitCaffeineInstance.java
@@ -0,0 +1,28 @@
+package com.example.apputils.cache;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @Description: TODO
+ * @author: wangwc
+ * @date: 2020/11/30 10:39
+ */
+@Configuration
+@Slf4j
+public class InitCaffeineInstance {
+    @Value("${level1.cache.initial.capacity:10000}")
+    private int initialCapacity;
+    @Value("${level1.cache.maximum.size:20000}")
+    private int maximumSize;
+
+    public Cache<String, Object> caffeine() {
+        log.info("============== Init CaffeineCache ==============");
+        Cache<String, Object> caffeineCache = Caffeine.newBuilder().initialCapacity(initialCapacity).maximumSize(maximumSize).build();
+        log.info("===> CaffeineCache => initialCapacity:{},maximumSize:{}", initialCapacity, maximumSize);
+        return caffeineCache;
+    }
+}
