ef59a7eb2595326c3add852443d668ed1d249f51
wangwc <wwc953@163.com>
2021-01-12 20:55:49 +0800

    黑名单--网关

--- app-gateway/src/main/java/com/example/appgateway/filter/BlackListFilter.java
+++ app-gateway/src/main/java/com/example/appgateway/filter/BlackListFilter.java
@@ -1,10 +1,13 @@
 package com.example.appgateway.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.core.Ordered;
 import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -16,6 +19,9 @@ import java.util.List;
  * @author: wangwc
  * @date: 2021/1/12 20:21
  */
+@RefreshScope
+@Component
+@Slf4j
 public class BlackListFilter implements GlobalFilter, Ordered {
 
     @Value("${appgetway.load.cache.level:1}")
