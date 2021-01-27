26ccd1397a30a47e8ada968a5fbccdc062e2c61e
wangwc <wwc953@163.com>
2021-01-27 15:08:20 +0800

    Feign上传文件

--- app-user/src/main/java/com/example/appuser/remote/IOrderServiceFeign.java
+++ app-user/src/main/java/com/example/appuser/remote/IOrderServiceFeign.java
@@ -2,16 +2,22 @@ package com.example.appuser.remote;
 
 import com.example.appuser.remote.impl.OrderServiceFeignImpl;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @FeignClient(name = "app-order", fallback = OrderServiceFeignImpl.class)
 public interface IOrderServiceFeign {
 
-    @GetMapping("/order/{param}")
+    @GetMapping(value = "/order/{param}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String callOrder(@PathVariable(value = "param") String param);
 
-    @GetMapping("/getOrder/{id}")
+    @GetMapping(value = "/getOrder/{id}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public String getOrderFromOrder(@PathVariable(value = "id") Integer id);
+
+    @RequestMapping(value = "/custevaluation/uploadFile", method = RequestMethod.POST,
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public String uploadFile(@RequestPart("file") MultipartFile multipartFile,
+                             @RequestParam("custId") String custId);
 
 }
--- app-user/src/main/java/com/example/appuser/remote/impl/OrderServiceFeignImpl.java
+++ app-user/src/main/java/com/example/appuser/remote/impl/OrderServiceFeignImpl.java
@@ -1,6 +1,7 @@
 package com.example.appuser.remote.impl;
 
 import com.example.appuser.remote.IOrderServiceFeign;
+import org.springframework.web.multipart.MultipartFile;
 
 public class OrderServiceFeignImpl implements IOrderServiceFeign {
     @Override
@@ -11,5 +12,10 @@ public class OrderServiceFeignImpl implements IOrderServiceFeign {
     @Override
     public String getOrderFromOrder(Integer id) {
         return "远程调用失败！！！";
+    }
+
+    @Override
+    public String uploadFile(MultipartFile multipartFile, String custId) {
+        return "远程调用失败!";
     }
 }
